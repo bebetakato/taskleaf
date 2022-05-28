1feca50b20864762fece9418c752d013a03a16dc
bebetakato <takato.baseball0227@gmail.com>
2022-05-28 22:09:25 +0900

    Task Complete

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -44,7 +44,11 @@ class Admin::UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :admin, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :admin, :password, :password_confirmation)
   end
 
+  def require_admin
+    redirect_to root_url unless current_user.admin?
+  end
+  
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,8 +1,10 @@
 class Task < ApplicationRecord
     validates :name, presence: true
-    validates :name, length: { maximum: 30 }
     validate :validate_name_including_comma
-    before_validation :set_nameless_name
+
+    belongs_to :user
+
+    scope :recent, -> { order(created_at: :desc) }
 
     private
 
